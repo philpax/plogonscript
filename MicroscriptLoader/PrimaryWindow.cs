9ddcadac43fce507306af9f04855f21c0b45d878
Philpax <me@philpax.me>
2022-04-17 01:53:46 +0200

    Move menu to top-level window

--- MicroscriptLoader/PrimaryWindow.cs
+++ MicroscriptLoader/PrimaryWindow.cs
@@ -16,27 +16,99 @@ internal class PrimaryWindow : Window
     {
         _scriptManager = scriptManager;
         _configuration = configuration;
-        
+
         Size = new Vector2(1200, 675);
         SizeCondition = ImGuiCond.FirstUseEver;
+        Flags |= ImGuiWindowFlags.MenuBar;
     }
 
-    private string? SelectedScript
+    private string? SelectedScriptName
     {
         get => _configuration.SelectedScript;
         set => _configuration.SelectedScript = value;
     }
 
+    private Script? SelectedScript
+    {
+        get
+        {
+            if (SelectedScriptName == null) return null;
+
+            _scriptManager.Scripts.TryGetValue(SelectedScriptName, out var script);
+            return script;
+        }
+    }
+
+    public override void OnOpen()
+    {
+        base.OnOpen();
+
+        if (SelectedScriptName == null || !_scriptManager.Scripts.ContainsKey(SelectedScriptName))
+            SelectedScriptName = _scriptManager.Scripts.Keys.FirstOrDefault();
+    }
+
     public override void Draw()
     {
-        if (ImGui.Button("Open Scripts Folder")) _scriptManager.OpenFolder();
-
-        if (SelectedScript == null || !_scriptManager.Scripts.ContainsKey(SelectedScript))
-            SelectedScript = _scriptManager.Scripts.Keys.FirstOrDefault();
-
+        DrawMenuBar();
         DrawLeftPane();
         ImGui.SameLine();
         DrawRightPane();
+    }
+
+    private void DrawMenuBar()
+    {
+        if (!ImGui.BeginMenuBar()) return;
+
+        if (ImGui.BeginMenu("File"))
+        {
+            if (ImGui.MenuItem("Open Scripts Folder"))
+                _scriptManager.OpenFolder();
+
+            if (SelectedScript != null)
+            {
+                if (ImGui.MenuItem("Save", "CTRL+S"))
+                    SelectedScript.SaveContents();
+            }
+
+            ImGui.EndMenu();
+        }
+
+        if (SelectedScript != null && ImGui.BeginMenu("Script"))
+        {
+            var originalAutoload = _configuration.AutoloadedScripts.GetValueOrDefault(SelectedScript.Name);
+            var imguiAutoload = originalAutoload;
+            ImGui.Checkbox("Autoload", ref imguiAutoload);
+            if (imguiAutoload != originalAutoload)
+            {
+                _configuration.AutoloadedScripts[SelectedScript.Name] = imguiAutoload;
+                _configuration.Save();
+            }
+
+            if (SelectedScript.Loaded)
+            {
+                if (ImGui.MenuItem("Reload Script"))
+                {
+                    SelectedScript.Unload();
+                    SelectedScript.Load();
+                }
+
+                if (ImGui.MenuItem("Unload Script"))
+                {
+                    SelectedScript.Unload();
+                }
+            }
+            else
+            {
+                if (ImGui.MenuItem("Load Script"))
+                {
+                    SelectedScript.Load();
+                }
+            }
+
+            ImGui.EndMenu();
+        }
+
+        ImGui.EndMenuBar();
     }
 
     private void DrawLeftPane()
@@ -45,8 +117,8 @@ internal class PrimaryWindow : Window
 
         foreach (var script in _scriptManager.Scripts.Values)
         {
-            if (ImGui.Selectable(script.Name, script.Name == SelectedScript))
-                SelectedScript = script.Name;
+            if (ImGui.Selectable(script.Name, script.Name == SelectedScriptName))
+                SelectedScriptName = script.Name;
             ImGui.SameLine();
             ImGui.PushStyleColor(ImGuiCol.Button,
                 script.Loaded
@@ -59,61 +131,22 @@ internal class PrimaryWindow : Window
         ImGui.EndChild();
     }
 
+
     private void DrawRightPane()
     {
         // Leave room for 1 line below us
-        ImGui.BeginChild("item view",
-            new Vector2(0, 0), true, ImGuiWindowFlags.MenuBar);
-        DrawSelectedScript();
+        ImGui.BeginChild("item view", new Vector2(0, 0), true);
+        DrawScript();
         ImGui.EndChild();
     }
 
-    private void DrawSelectedScript()
+    private void DrawScript()
     {
-        Script? script = null;
-        if (SelectedScript != null)
-            _scriptManager.Scripts.TryGetValue(SelectedScript, out script);
-        if (script == null) return;
+        if (SelectedScript == null) return;
 
-        DrawMenuBar(script);
-        var contents = script.Contents;
+        var contents = SelectedScript.Contents;
         ImGui.InputTextMultiline("##source", ref contents, 16384,
             new Vector2(-float.Epsilon, -float.Epsilon), ImGuiInputTextFlags.AllowTabInput);
-        script.Contents = contents;
-    }
-
-    private void DrawMenuBar(Script script)
-    {
-        if (!ImGui.BeginMenuBar()) return;
-
-        if (ImGui.MenuItem("Save", "CTRL+S"))
-            script.SaveContents();
-
-        var originalAutoload = _configuration.AutoloadedScripts.GetValueOrDefault(script.Name);
-        var imguiAutoload = originalAutoload;
-        ImGui.Checkbox("Autoload", ref imguiAutoload);
-        if (imguiAutoload != originalAutoload)
-        {
-            _configuration.AutoloadedScripts[script.Name] = imguiAutoload;
-            _configuration.Save();
-        }
-
-        if (script.Loaded)
-        {
-            if (ImGui.MenuItem("Reload Script"))
-            {
-                script.Unload();
-                script.Load();
-            }
-
-            ImGui.SameLine();
-            if (ImGui.MenuItem("Unload Script")) script.Unload();
-        }
-        else
-        {
-            if (ImGui.MenuItem("Load Script")) script.Load();
-        }
-
-        ImGui.EndMenuBar();
+        SelectedScript.Contents = contents;
     }
 }
