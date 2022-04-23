7ba9de4049066218f67e260e2c0f8ec0651ffed7
Philpax <me@philpax.me>
2022-04-23 21:00:03 +0200

    Events: add missing OnKeyUp

--- PlogonScript/Script/Events.cs
+++ PlogonScript/Script/Events.cs
@@ -1,5 +1,3 @@
-using System;
-using System.Collections.Generic;
 using Dalamud.Game.ClientState.Keys;
 using Dalamud.Game.Text;
 using Dalamud.Game.Text.SeStringHandling;
@@ -15,16 +13,24 @@ public static class Events
 
     public static readonly Event OnKeyUp = new("onKeyUp", ("key", typeof(VirtualKey)));
 
-    public static readonly Event OnChatMessageHandled = new("onChatMessageHandled", 
-        ("type", typeof(XivChatType)), ("senderId", typeof(uint)), 
+    public static readonly Event OnChatMessageHandled = new("onChatMessageHandled",
+        ("type", typeof(XivChatType)), ("senderId", typeof(uint)),
         ("sender", typeof(SeString)), ("message", typeof(SeString))
     );
 
-    public static readonly Event OnChatMessageUnhandled = new("onChatMessageUnhandled", 
-        ("type", typeof(XivChatType)), ("senderId", typeof(uint)), 
+    public static readonly Event OnChatMessageUnhandled = new("onChatMessageUnhandled",
+        ("type", typeof(XivChatType)), ("senderId", typeof(uint)),
         ("sender", typeof(SeString)), ("message", typeof(SeString))
     );
 
     public static readonly Event[] AllEvents =
-        {OnLoad, OnUnload, OnDraw, OnUpdate, OnChatMessageHandled, OnChatMessageUnhandled};
+    {
+        OnLoad,
+        OnUnload,
+        OnDraw,
+        OnUpdate,
+        OnKeyUp,
+        OnChatMessageHandled,
+        OnChatMessageUnhandled
+    };
 }
